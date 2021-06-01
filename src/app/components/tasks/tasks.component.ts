17ad88b55487c563111c8ff0c58aea3b9201562d
prakash <prkjha25@gmail.com>
2021-06-01 20:18:18 +0530

    submit and create tasks

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -24,4 +24,7 @@ export class TasksComponent implements OnInit {
     this.taskService.updateTaskReminder(task).subscribe();
     console.log(task);
   }
+  addTask(task: Task) {
+    this.taskService.addTask(task).subscribe((task) => this.tasks.push(task));
+  }
 }
